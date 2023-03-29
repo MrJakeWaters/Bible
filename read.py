30204b80cc1b9190a1086ecd9b4424126e88e32a
MrJakeWaters <waters.1909@gmail.com>
2023-03-28 22:27:13 -0600

    reformatting cleaner code

--- read.py
+++ read.py
@@ -2,18 +2,25 @@ import random
 import xml.etree.ElementTree as ET
 tree = ET.parse('scripture.xml')
 root = tree.getroot()
-random_book_index = random.randint(1,65)
-i = 1
-for book in root.iter('chapters'):
-    chapters = int(book.text)
-    if i == random_book_index:
-        random_chapter = random.randint(1,chapters)
-    i = i + 1
 
-i = 1
-for book in root.iter('name'):
-    if i == random_book_index:
-        random_book = book.text
-    i = i + 1
+class Scripture:
+    def __init__(self, tree):
+        self.tree = tree
+        self.getRandomChapter()
+        
+    def setRandomBookIndex(self):
+        # generates random number between 1 and 65
+        self.random_book_index = random.randint(1,65)
 
-print("%s:%s" % (random_book, str(random_chapter)))
+    def getRandomChapter(self):
+        self.setRandomBookIndex() # set random book number index
+        chapters = [x.text for x in self.tree.iter('chapters')]
+        self.chapter = chapters[self.random_book_index]
+        self.setRandomBookName()
+
+    def setRandomBookName(self):
+        books = [x.text for x in self.tree.iter('name')]
+        self.book = books[self.random_book_index]
+
+ref = Scripture(root)
+print("%s:%s" % (ref.book, str(ref.chapter)))
